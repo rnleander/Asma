aa24be61d4dd4d7a13ea14cd7eee339833d7bf0a
John Ford <jrf2m@mtmail.mtsu.edu>
2019-03-29 14:02:08 -0500

    Fixed incorrect parameters in invgcdf normal distrubtion cdf Simplified inverse_Gaussian_tp3 Added mathematica notebook transition_probability.nb for exploring the issue analytically

--- inverse_Gaussian_tp3.m
+++ inverse_Gaussian_tp3.m
@@ -1,10 +1,11 @@
-function [y] = inverse_Gaussian_tp3(a,m,s)
+function y = inverse_Gaussian_tp3(a,m,s)
 %computes the transition probability at the ages in a
 
-y1=onestagepdf2(a,m,s);
+%y1=onestagepdf2(a,m,s);
 
-int = invgcdf(a,m,s);
 
-y = y1./(1.-int);
+%y = y1./(1.-invgcdf(a,m,s));
 
+
+y = onestagepdf2(a,m,s)./(1-invgcdf(a,m,s));
 end
--- invgcdf.m
+++ invgcdf.m
@@ -8,9 +8,9 @@ arg1 = sqrt(lam./t).*(t./mu - 1);
 
 arg2 = -1.*sqrt(lam./t).*(t./mu + 1);
 
-norm1 = cdf('Normal',arg1,1,1);
+norm1 = cdf('Normal',arg1,0,1);
 
-norm2 = cdf('Normal',arg2,1,1);
+norm2 = cdf('Normal',arg2,0,1);
 
 y = norm1+exp(2*lam/mu)*norm2;
 
