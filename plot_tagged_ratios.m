36204e0cff7e78d1a5447e7cbe2e37c59f732205
John Ford <jrf2m@mtmail.mtsu.edu>
2019-04-11 16:12:25 -0500

    plot_tagged_ratios now does kmeans clustering of the FUCCI data
    There are several different things I was playing with here so the file has become disorganized. I should probably consider breaking different analyses into their own functions.

--- plot_tagged_ratios.m
+++ plot_tagged_ratios.m
@@ -4,15 +4,68 @@ A = readtable(filename);
 h = 0.1;
 idx = 1;
 
+g1_total = 0;
+g2_total = 0;
+birth_g1 = zeros(120/h,120/h);
+
+clf
+
+hold on
 for row=1:size(A,1)
-    birthtime = table2array(A(row,'Birthtimeh'));
-    g1_time = table2array(A(row,'G1_time'));
-    g2_time = table2array(A(row,'S_G2_M_time'));
-    g1_start(row) = birthtime;
-    g2_start(row) = birthtime+g1_time;
+    birthtime(row) = table2array(A(row,'Birthtimeh'));
+    g1_time(row) = table2array(A(row,'G1_time'));
+    g2_time(row) = table2array(A(row,'S_G2_M_time'));
+    g1_start(row) = birthtime(row);
+    g2_start(row) = birthtime(row)+g1_time(row);
     g1_end(row) = g2_start(row);
-    g2_end(row) = g2_start(row)+g2_time;
+    g2_end(row) = g2_start(row)+g2_time(row);
+    g1_total = g1_total + g1_time(row);
+    g2_total = g2_total + g2_time(row);
+    
+    birthIdx = round(birthtime(row)/h)+1;
+    g1Idx = round(g1_time(row)/h)+1;
+    
+    birth_g1(birthIdx,g1Idx) = birth_g1(birthIdx,g1Idx) + 1;
+    
+    X(row,1) = birthtime(row);
+    X(row,2) = g1_time(row);
+    X(row,3) = g2_time(row);
 end
+g1_average = g1_total/size(A,1);
+g2_average = g2_total/size(A,1);
+
+idx = kmeans(X,3);
+
+hold on
+view(40,35)
+plot3(X(idx==1,1),X(idx==1,2),X(idx==1,3),'r.','MarkerSize',12)
+plot3(X(idx==2,1),X(idx==2,2),X(idx==2,3),'b.','MarkerSize',12)
+plot3(X(idx==3,1),X(idx==3,2),X(idx==3,3),'g.','MarkerSize',12)
+title('3 Kmeans clusters in Birth time, G1 time and G2 time')
+xlabel('Birth time')
+ylabel('G1 time')
+zlabel('G2 time')
+
+return
+
+scatter3(birthtime, g1_time,g2_time,'o')
+
+
+return
+
+heatmap(A,'Birthtimeh','S_G2_M_time')
+
+return
+
+birth_g1
+hist3(birth_g1)
+return
+
+histogram(g2_time, 'BinWidth', 0.1)
+title('Frequency distribution of G2 duration in FUCCI cells')
+xlabel('G2 duration')
+ylabel('Count')
+return
 
 for time=0:h:120
     t(idx)=time;
