03cc0d67b58d870ac42c64b80e89a8360da65a6e
rleander <rleander@10.82.72.19>
2019-04-12 12:01:57 -0500

    updated code to use ML parameters from the untreated MCF10A cell data for simulating the population with the inverse Gaussian or exponential transition rates.

--- cell_pop_beta.m
+++ cell_pop_beta.m
@@ -4,38 +4,53 @@ function [beta1, beta2] = cell_pop_beta(a1max,a2max,T,hfine,beta)
 %cells that leave the second stage, divide, and their daughters enter the
 %first stage.
 
+%We will use parameters that correspond to the ML parameters for untreated 
+%MCF10A cells. mu1=.25, sigma1=1, mu2=.064, sigma2=.031. Using the
+%mean=1/mu and var=sigma^2/mu^3.
+
+mu1=.25;
+sigma1=1;
+mu2=.064;
+sigma2=.031;
+
+mean1=1/mu1;
+var1=sigma1^2/mu1^3;
+mean2=1/mu2;
+var2=sigma2^2/mu2^3;
+
 ages1_fine=0:hfine:a1max+T;
 ages2_fine=0:hfine:a2max+T;
 
-if strcmp(beta,'gaussian')
-% Transition probabilities have an age-dependency with Gaussian dist.
-    mu=10;
-    sigmasq=1;
-    B1=10;
-    B2=100;
-    beta1=Gaussian_density(ages1_fine,mu,sigmasq);
-    beta2=Gaussian_density(ages2_fine,mu,sigmasq);
-
-    beta1=B1*beta1;
-    beta2=B2*beta2;
-
-end
+%This option is currently nonfunctional
+% if strcmp(beta,'gaussian')
+% % Transition probabilities have an age-dependency with Gaussian dist.
+%     mu=10;
+%     sigmasq=1;
+%     B1=10;
+%     B2=100;
+%     beta1=Gaussian_density(ages1_fine,mu,sigmasq);
+%     beta2=Gaussian_density(ages2_fine,mu,sigmasq);
+% 
+%     beta1=B1*beta1;
+%     beta2=B2*beta2;
+% 
+% end
 
 if strcmp(beta,'exponential')
-    B1=10;
-    B2=5;
+    lambda1=1/mean1;
+    lambda2=1/mean2;
     beta1=ones(size(ages1_fine));
     beta2=ones(ages2_fine);
 
-    beta1=B1*beta1;
-    beta2=B2*beta2;
+    beta1=lambda1*beta1;
+    beta2=lambda2*beta2;
 
 end
 
 if strcmp(beta,'inverse_gaussian')
 
-    beta1=inverse_Gaussian_tp3(ages1_fine,.08,.02);
-    beta2=inverse_Gaussian_tp3(ages2_fine,.08,.02);
+    beta1=inverse_Gaussian_tp3(ages1_fine,mu1,sigma1);
+    beta2=inverse_Gaussian_tp3(ages2_fine,mu2,sigma2);
 
 
 end
